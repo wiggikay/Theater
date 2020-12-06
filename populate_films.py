81174a0e2f4fe61eb422028eee310cc05d8d8f42
Kayla Wiggins <wiggikay@umich.edu>
2020-12-06 17:46:11 -0500

    Added films to the database

--- populate_films.py
+++ populate_films.py
@@ -3,17 +3,14 @@ import json
 import sqlite3
 
 ############# The db file ##############
-db_file = 'filmography.sqlite'
+db_file = 'popular_actors1.db'
 conn = sqlite3.connect(db_file)
 cur = conn.cursor()
 
 #cur.execute('CREATE TABLE IF NOT EXISTS Actors (actor_id INTEGER PRIMARY KEY, name TEXT, gender INTEGER, films TEXT, avg_rating INTEGER, fav_genre INTEGER)')
-#cur.execute('CREATE TABLE IF NOT EXISTS Films (film_id INTEGER PRIMARY KEY, name TEXT, genres TEXT, rating INTEGER, release_date TEXT)')
+cur.execute('CREATE TABLE IF NOT EXISTS Films (film_id INTEGER PRIMARY KEY, name TEXT, genres TEXT, rating INTEGER, release_date TEXT)')
 
-#cur.execute('INSERT INTO Actors (actor_id, name, gender) VALUES (?,?,?)', (31, 'Tom Hanks', 2))
-#cur.execute('INSERT INTO Actors (actor_id, name, gender) VALUES (?,?,?)', (9780, 'Angela Bassett', 1))
-#cur.execute('INSERT INTO Actors (actor_id, name, gender) VALUES (?,?,?)', (18918, 'The Rock', 2))
-#conn.commit()
+conn.commit()
 #########################################
 key = 'eff108aecbb73d143e06dc125caf045f'
 base = 'https://api.themoviedb.org/3/discover/movie?api_key=eff108aecbb73d143e06dc125caf045f&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_cast='
@@ -26,7 +23,11 @@ def pull_films(info):
     added to the films attribute of the actor's tuple in the database.
     '''
     filmography = []
-    for film in range(10):
+    film_list = len(info["results"])
+    if film_list >= 10:
+        film_list = 10
+
+    for film in range(film_list):
         pos = info["results"][film]
         identity = pos["id"]
         name = pos["original_title"]
@@ -37,7 +38,10 @@ def pull_films(info):
             genres.append(str(genre))
 
         votes = pos["vote_average"]
-        date = pos["release_date"]
+        if "release_date" in pos:
+            date = pos["release_date"]
+        else:
+            date = ""
         cur.execute('INSERT or IGNORE INTO Films (film_id, name, genres, rating, release_date) VALUES (?, ?, ?, ?, ?)', (identity, name, ",".join(genres), votes, date))
         filmography.append(str(identity))
     conn.commit()
@@ -49,7 +53,7 @@ def call_actors():
     Selects all of the actors from the database and adds a list of 10 of their best rated films 
     to their films attribute.
     '''
-    cur.execute('SELECT actor_id, name FROM Actors')
+    cur.execute('SELECT actor_id, actor_name FROM Actors')
     disk = []
     for actor in cur:
         disk.append(actor)
@@ -59,7 +63,7 @@ def call_actors():
         full_req = base + str(actor_id)
         response = requests.get(full_req)
         cata = json.loads(response.content)
-        cur.execute('UPDATE Actors SET films= ? WHERE actor_id= ?', (",".join(pull_films(cata)), actor_id))
+        cur.execute('UPDATE Actors SET actor_films= ? WHERE actor_id= ?', (",".join(pull_films(cata)), actor_id))
         conn.commit()
         
 
